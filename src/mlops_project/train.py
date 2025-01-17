434cb89385306f851f29bd39c84b18584b10993e
mariatmvendas <s242906@dtu.dk>
2025-01-17 16:56:48 +0100

    work directory updated train.py

--- src/mlops_project/train.py
+++ src/mlops_project/train.py
@@ -166,7 +166,7 @@ def train(
 
         typer.echo(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss / len(dataloader):.4f}")
 
-    torch.save(model.state_dict(), "../../models/model.pth")
+    torch.save(model.state_dict(), "models/model.pth")
     typer.echo("Training complete! Model saved as model.pth and run logged on W&B.")
 
 @app.command()
