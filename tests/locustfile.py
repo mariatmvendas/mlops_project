266cfcd56cd480e59ff66d535b5b5eebce6206b1
ric565 <s241925@dtu.dk>
2025-01-22 16:44:30 +0100

    comment tests

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -1,3 +1,26 @@
+"""
+Locust Performance Testing Script for FastAPI Application
+
+This script uses Locust, a load testing tool, to simulate user interactions with a FastAPI application.
+It defines tasks to test two endpoints of the application:
+1. `GET /`: Simulates users visiting the root endpoint to retrieve API instructions.
+2. `POST /inference_satellite/`: Simulates users uploading an image to get a predicted label from the satellite image classification model.
+
+Key Features:
+- The script simulates concurrent users making requests to both endpoints.
+- For the `/inference_satellite/` endpoint, it sends a test image (`desert.jpg`) as part of the request.
+- It logs failed requests for debugging purposes.
+
+How to Use:
+1. locust -f locustfile.py --host http://127.0.0.1:8000
+2. open interface at http://127.0.0.1:8089
+
+Requirements:
+1. A running FastAPI application at http://127.0.0.1:8000.
+2. A test image located at ./tests/desert.jpg. 
+
+"""
+
 from locust import HttpUser, between, task
 
 class MyUser(HttpUser):
@@ -21,6 +44,5 @@ class MyUser(HttpUser):
                 "/inference_satellite/",
                 files={"data": image_file},
             )
-            # Optionally, print or log the response for debugging
             if response.status_code != 200:
                 print(f"Failed request: {response.status_code}, {response.text}")
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,26 @@
 """
-export MYENDPOINT=http://localhost:8000
+Unit Test for the Root Endpoint of the FastAPI Application
+
+This script tests the `/` (root) endpoint of the FastAPI application using TestClient.
+The root endpoint is expected to:
+1. Be accessible via a GET request.
+2. Return a 200 OK status code.
+3. Provide a JSON response containing a welcome message and instructions for using the API.
+
+Key Features of the Test:
+- Verifies that the response contains the correct HTTP status code.
+- Checks that the JSON response includes:
+  - A "message" key with a predefined welcome message.
+  - An "instructions" key with relevant keys for API usage details.
+- Ensures the structure and content of the API response align with expectations.
+
+How to Use:
+1.Run the API
+2. Run the test using a testing framework like `pytest`:
+   ```bash
+   export MYENDPOINT=http://localhost:8000
+   pytest path_to_test_file.py
+
 """
 from fastapi.testclient import TestClient
 from src.mlops_project.api import app
